730793e40bc95f752d4a8a7ae2d4b6b8760b4b47
Byron Seto <byronseto@hotmail.com>
2017-03-10 15:04:10 -0700

    Saving right before changing all sockets to hash

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var crypto = require('crypto');
 
 var adjectives = ["fluttering", "little", "certain", "regular", "lopsided", "lumpy", "trite", "mountainous", "half", "determined", "tasteless", "breezy", "royal", "great", "familiar", "zippy", "fascinated", "erect", "probable", "useful", "purple", "terrible", "windy", "lovely", "outstanding", "weary", "curvy", "taboo", "aquatic", "cloistered"]
 
@@ -12,107 +13,124 @@ var users = new Map();
 var queueHistory = [];
 
 http.listen( port, function () {
-    console.log('listening on port', port);
+	console.log('listening on port', port);
 });
 
 app.use(express.static(__dirname + '/public'));
 
 // listen to 'chat' messages
 io.on('connection', function(socket){
-    users.set(socket, {
-        NickName: generateName(),
-        Color: "#3366ff"
-    });
-    //To self only
-    socket.emit('history', queueHistory);
-    socket.emit('chat', generateMessage(socket, "system", "nolog", "Welcome! Your automatically assigned nickname is "+ users.get(socket).NickName));
-    //Everyone Else
-    socket.broadcast.emit('chat', generateMessage(socket, "system", "log", users.get(socket).NickName + " has joined the room."));
 
-    //Change all user lists
+	var cookie;
+	socket.emit('grabCookie', "chatroom");
+	socket.on('grabCookie', function(receive_cookie){
+		console.log(cookie);
+		cookie = receive_cookie;
+	});
 
-    io.emit('clearUserList');
-    users.forEach(function(user, key){
-        io.emit('updateUserList', user);
-    });
+	if(!cookie) {
+		var current_date = (new Date()).valueOf().toString();
+		var random = Math.random().toString();
+		var hash = crypto.createHash('sha1').update(current_date + random).digest('hex');
+		socket.emit('setCookie', "chatroom", hash, 1);
+	} else {
 
-    socket.on('chat', function(msg){
-        var command = checkSpecialCommand(socket, msg, io);
-        if (command == false) {
-            socket.emit('chat', generateMessage(socket, "user", "nolog", msg));
-            socket.broadcast.emit('chat', generateMessage(socket, "chat", "log", msg));
-        } else {
-            io.emit('chat', command);
-        }
-    });
+	}
 
-    socket.on('disconnect', function(){
-        io.emit('chat', generateMessage(socket, "chat", "log", "disconnected"));
-        users.delete(socket);
-    });
+
+	users.set(socket, {
+		NickName: generateName(),
+		Color: "#3366ff"
+	});
+	//To self only
+	socket.emit('history', queueHistory);
+	socket.emit('chat', generateMessage(socket, "system", "nolog", "Welcome! Your automatically assigned nickname is "+ users.get(socket).NickName));
+	//Everyone Else
+	socket.broadcast.emit('chat', generateMessage(socket, "system", "log", users.get(socket).NickName + " has joined the room."));
+
+	//Change all user lists
+
+	io.emit('clearUserList');
+	users.forEach(function(user, key){
+		io.emit('updateUserList', user);
+	});
+
+	socket.on('chat', function(msg){
+		var command = checkSpecialCommand(socket, msg, io);
+		if (command === false) {
+			socket.emit('chat', generateMessage(socket, "user", "nolog", msg));
+			socket.broadcast.emit('chat', generateMessage(socket, "chat", "log", msg));
+		} else {
+			io.emit('chat', command);
+		}
+	});
+
+	socket.on('disconnect', function(){
+		io.emit('chat', generateMessage(socket, "chat", "log", "disconnected"));
+		users.delete(socket);
+	});
 });
 function generateMessage(socket, type, logbool, message) {
-    var message = {
-        MessageType: type,
-        Message: message,
-        UserInfo: users.get(socket),
-        TimeStamp: timestamp()
-    }
+	var message = {
+		MessageType: type,
+		Message: message,
+		UserInfo: users.get(socket),
+		TimeStamp: timestamp()
+	}
 
-    if (logbool.localeCompare("log") == 0) {
-        queueHistory.push(message);
-        if (queueHistory.length > 200) {
-            queueHistory.slice(0, 201)
-        }
-    }
-    return message;
+	if (logbool.localeCompare("log") === 0) {
+		queueHistory.push(message);
+		if (queueHistory.length > 200) {
+			queueHistory.slice(0, 201)
+		}
+	}
+	return message;
 }
 
 function checkSpecialCommand(socket, message) {
-    var command = message.split(" ")[0];
-    switch(command) {
-        case "/nick":
-            if (users.has(message.substring(6, message.length))){
-                return generateMessage(socket, "system", "nolog", message.substring(6, message.length) + " Nick name already exists! Please pick a different nick name");
-            } else {
-                oldUserInfo = users.get(socket);
-                oldName = oldUserInfo.NickName;
-                users.delete(socket);
-                oldUserInfo.NickName = message.substring(6, message.length);
-                users.set(socket, oldUserInfo);
-                io.emit('clearUserList');
-                users.forEach(function(user, key){
-                    io.emit('updateUserList', user);
-                });
-                return generateMessage(socket, "system", "log", oldName + " has changed their nick name to " + users.get(socket).NickName);
-            }
-            break;
-        case "/nickcolor":
-            oldUserInfo = users.get(socket);
-            oldColor = oldUserInfo.Color;
-            users.delete(socket);
-            oldUserInfo.Color = "#"+message.substring(11, 18);
-            users.set(socket, oldUserInfo);
-            return generateMessage(socket, "chat", "log", users.get(socket).NickName + " has changed their color to " + oldColor);
-    }
-    return false;
+	var command = message.split(" ")[0];
+	switch(command) {
+		case "/nick":
+			if (users.has(message.substring(6, message.length))){
+				return generateMessage(socket, "system", "nolog", message.substring(6, message.length) + " Nick name already exists! Please pick a different nick name");
+			} else {
+				oldUserInfo = users.get(socket);
+				oldName = oldUserInfo.NickName;
+				users.delete(socket);
+				oldUserInfo.NickName = message.substring(6, message.length);
+				users.set(socket, oldUserInfo);
+				io.emit('clearUserList');
+				users.forEach(function(user, key){
+					io.emit('updateUserList', user);
+				});
+				return generateMessage(socket, "system", "log", oldName + " has changed their nick name to " + users.get(socket).NickName);
+			}
+			break;
+		case "/nickcolor":
+			oldUserInfo = users.get(socket);
+			oldColor = oldUserInfo.Color;
+			users.delete(socket);
+			oldUserInfo.Color = "#"+message.substring(11, 18);
+			users.set(socket, oldUserInfo);
+			return generateMessage(socket, "chat", "log", users.get(socket).NickName + " has changed their color to " + oldColor);
+	}
+	return false;
 }
 
 
 function timestamp() {
-    return (new Date).toISOString().replace(/z|t/gi,' ').trim().substring(11,19);
+	return (new Date).toISOString().replace(/z|t/gi,' ').trim().substring(11,19);
 }
 
 function generateName() {
-    var name = randomElement(adjectives) + "-" + randomElement(nouns);
-    while(users.has(name)) {
-        name = randomElement(adjectives) + "-" + randomElement(nouns);
-    }
-    return name;
+	var name = randomElement(adjectives) + "-" + randomElement(nouns);
+	while(users.has(name)) {
+		name = randomElement(adjectives) + "-" + randomElement(nouns);
+	}
+	return name;
 }
 
 function randomElement(list) {
-    var index = Math.floor(Math.random() * list.length);
-    return list[index]
+	var index = Math.floor(Math.random() * list.length);
+	return list[index]
 }
-
--- public/client.js
+++ public/client.js
@@ -1,80 +1,107 @@
 
 // shorthand for $(document).ready(...)
 $(function() {
-    var socket = io();
-    $('form').submit(function(){
-        socket.emit('chat', $('#m').val());
-        $('#m').val('');
-        return false;
-    });
-    socket.on('chat', function(msg){
-        if (msg.MessageType) {
-            switch(msg.MessageType) {
-                case "system":
-                    System(msg);
-                    break;
-                case "chat":
-                    Message(msg);
-                    break;
-                case "user":
-                    UserMessage(msg);
-                    break;
-            }
-        }
-    });
+	var socket = io();
+	$('form').submit(function(){
+		socket.emit('chat', $('#m').val());
+		$('#m').val('');
+		return false;
+	});
 
-    socket.on('history', function(queueHistory){
-        for(msg of queueHistory) {
-            if (msg.MessageType) {
-                switch(msg.MessageType) {
-                    case "system":
-                        System(msg);
-                        break;
-                    case "chat":
-                        Message(msg);
-                        break;
-                    case "user":
-                        UserMessage(msg);
-                        break;
-                }
-            }
-        }
-    });
+	//From https://www.w3schools.com/js/js_cookies.asp
+	socket.on('grabCookie', function(cname){
+		var name = cname + "=";
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split(';');
+		for(var i = 0; i <ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == ' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				socket.emit('grabCookie', c.substring(name.length, c.length));
+			}
+		}
+		socket.emit('grabCookie', "");
+	});
 
-    socket.on('clearUserList', function(user) {
-        $('#users').empty();
-    });
+	//From https://www.w3schools.com/js/js_cookies.asp
+	socket.on('setCookie', function(name, value, days){
+		var d = new Date();
+		d.setTime(d.getTime() + (days*24*60*60*1000));
+		var expires = "expires="+ d.toUTCString();
+		document.cookie = name + "=" + value + ";path=/";
+	});
 
-    socket.on('updateUserList', function(user) {
-        $('#users').prepend($('<li>').html(user.NickName));
-    });
+	socket.on('chat', function(msg){
+		if (msg.MessageType) {
+			switch(msg.MessageType) {
+				case "system":
+					System(msg);
+					break;
+				case "chat":
+					Message(msg);
+					break;
+				case "user":
+					UserMessage(msg);
+					break;
+			}
+		}
+	});
+
+	socket.on('history', function(queueHistory){
+		for(msg of queueHistory) {
+			if (msg.MessageType) {
+				switch(msg.MessageType) {
+					case "system":
+						System(msg);
+						break;
+					case "chat":
+						Message(msg);
+						break;
+					case "user":
+						UserMessage(msg);
+						break;
+				}
+			}
+		}
+	});
+
+
+	socket.on('clearUserList', function(user) {
+		$('#users').empty();
+	});
+
+	socket.on('updateUserList', function(user) {
+		$('#users').prepend($('<li>').html(user.NickName));
+	});
 
 });
 function System(msg){
-    var message = '<b>' + msg.TimeStamp + ' ';
-    message += "<span style='color:#34495E;'>System</b> : </span>";
-    message += msg.Message;
-    $('#messages').prepend($('<li>').html(message));
-    var messageDiv = document.getElementById("messages");
-    messageDiv.scrollTop = messageDiv.scrollHeight;
+	var message = '<b>' + msg.TimeStamp + ' ';
+	message += "<span style='color:#34495E;'>System</b> : </span>";
+	message += msg.Message;
+	$('#messages').prepend($('<li>').html(message));
+	var messageDiv = document.getElementById("messages");
+	messageDiv.scrollTop = messageDiv.scrollHeight;
 }
 
 function Message(msg){
-    var message = '<b>' + msg.TimeStamp + '</b> ';
-    message += "<span style='color:" + msg.UserInfo.Color + ";'>" + msg.UserInfo.NickName + ": </span>";
-    message += msg.Message;
-    $('#messages').prepend($('<li>').html(message));
-    var messageDiv = document.getElementById("messages");
-    messageDiv.scrollTop = messageDiv.scrollHeight;
+	var message = '<b>' + msg.TimeStamp + '</b> ';
+	message += "<span style='color:" + msg.UserInfo.Color + ";'>" + msg.UserInfo.NickName + ": </span>";
+	message += msg.Message;
+	$('#messages').prepend($('<li>').html(message));
+	var messageDiv = document.getElementById("messages");
+	messageDiv.scrollTop = messageDiv.scrollHeight;
 }
 
 function UserMessage(msg){
-    var message = '<b>' + msg.TimeStamp + ' ';
-    message += "<span style='color:" + msg.UserInfo.Color + ";'>" + msg.UserInfo.NickName + ": </span>";
-    message += msg.Message + '</b>';
-    $('#messages').prepend($('<li>').html(message));
-    var messageDiv = document.getElementById("messages");
-    messageDiv.scrollTop = messageDiv.scrollHeight;
+	var message = '<b>' + msg.TimeStamp + ' ';
+	message += "<span style='color:" + msg.UserInfo.Color + ";'>" + msg.UserInfo.NickName + ": </span>";
+	message += msg.Message + '</b>';
+	$('#messages').prepend($('<li>').html(message));
+	var messageDiv = document.getElementById("messages");
+	messageDiv.scrollTop = messageDiv.scrollHeight;
 }
 
 
